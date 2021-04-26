f5e639cca9b4abe4b3b3dd6b9b17bf6324997df0
Kevin C. Tofel <kctofel@gmail.com>
2021-04-26 16:13:01 -0400

    Added option for random server port

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/../build'))
 app.use(cors());
 app.use(express.json());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
